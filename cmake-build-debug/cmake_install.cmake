655e1a5917d7ae502b9241b6f4841442ecfdbbba
andreifkz <andreifkz990@gmail.com>
2023-11-18 21:20:06 +0200

    comit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Andrei/CLionProjects/sneic
+# Install script for directory: C:/Users/Andrei/CLionProjects/sneictest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Andrei/CLionProjects/sneic/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Andrei/CLionProjects/sneictest/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Worldwwwwwwwwwwwww!" << std::endl;
     return 0;
 }
